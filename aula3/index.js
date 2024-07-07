d362f428008e60fcf5df81cf6006a95cf169175f
lid <lidson@oriontec.com.br>
2024-07-07 18:12:48 -0300

    Primeiras aulas

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Lidson "Oliveira"'); // String
+console.log("'Lidson' Oliveira");
+console.log(`Lidson Oliveira`);
+
+console.log(1); // Number / Int
+console.log(2.50); // Number / Float
+console.log(3, 5.60); 
